1a25ac098e740c2795efb4c2b55ffb8b2876126a
Roy Daniel <roydanielr@gmail.com>
2023-09-29 12:36:26 -0400

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,73 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app" v-if="fronteggLoaded">
+    <div v-if="authState.user">
+      <span>Logged in as: {{ authState.user.name }}</span>
+    </div>
+    <div>
+      <button v-if="authState.user" v-on:click="logout">Logout</button>
+      <button v-if="authState.user" v-on:click="showAccessToken">
+        What is my access token?
+      </button>
+      <button v-if="!authState.user" v-on:click="goToLogin">Login</button>
+      <button v-on:click="showAdminPortal">Open admin portal</button>
+
+      <!-- Conditionally render the div based on the user's roleIds -->
+      <div v-if="checkRoles()">
+        Private area for admins
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import {
+  useFrontegg,
+  ContextHolder,
+  useFronteggAuthGuard,
+  AdminPortal
+} from "@frontegg/vue";
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
-</script>
+  setup() {
+    const { fronteggLoaded, authState, loginWithRedirect } = useFrontegg();
+   
+    useFronteggAuthGuard(); // auto redirects the user to the login page / application
+    
+    const goToLogin = () => {
+      loginWithRedirect();
+    };
+    
+    const logout = () => {
+      const baseUrl = ContextHolder.getContext().baseUrl;
+      window.location.href = `${baseUrl}/oauth/logout?post_logout_redirect_uri=${window.location}`;
+    };
+    
+    const showAccessToken = () => {
+      alert(authState.user.accessToken);
+    };
+    
+    const showAdminPortal = () => {
+      AdminPortal.show();
+    };
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+    // Function to check if user has the required role
+    const checkRoles = () => {
+      return (
+        authState.user &&
+        authState.user.roles &&
+        authState.user.roles.some(role => role.id === 'test')
+      );
+    };
+
+    return {
+      fronteggLoaded,
+      authState,
+      goToLogin,
+      logout,
+      showAccessToken,
+      showAdminPortal,
+      checkRoles
+    };
+  },
+};
+</script>
--- src/main.js
+++ src/main.js
@@ -1,4 +1,27 @@
-import { createApp } from 'vue'
-import App from './App.vue'
+import { createApp } from "vue";
+import App from "./App.vue";
+import { Frontegg  } from "@frontegg/vue";
+import { createRouter, createWebHistory } from "vue-router";
 
-createApp(App).mount('#app')
+const router = createRouter({
+  history: createWebHistory("/"),
+  routes: [
+    { name: "HomePage", path: "/", component: App },
+  ],
+});
+
+const app = createApp(App).use(router);
+
+app.use(Frontegg, {
+  contextOptions: {
+    baseUrl: "baseUrl",
+    clientId: 'clientId'
+  },
+   authOptions: {
+	keepSessionAlive: true // Uncomment this in order to maintain the session alive
+  },
+  hostedLoginBox: false,
+  router,
+});
+
+app.mount("#app");
